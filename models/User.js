78c61f36d8b96eea6037809813e2473627972b1b
alarrabee <alexlarrabee10@gmail.com>
2024-06-29 19:51:02 -0500

    set up user model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+//Defines a User model for a database with fields for id, username and password. It includes a method for password comparison and a hook to hash passwords before saving them.
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+
+// compares a given password with the hashed password stored in the database
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6],
+        },
+      },
+    },
+    {
+      //hashes the password before saving a new user to the database.
+      hooks: {
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+      },
+      sequelize,
+      timestamps: true,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+
+  
+  
+  module.exports = User;
--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require('express'); //middleware, routing, HTTP requests, template engines, static file sharing, RESTfulAPIs
-const session = require('express-session'); //session management, session persistence, cookie handling
+// const session = require('express-session'); //session management, session persistence, cookie handling
 const exphbs = require('express-handlebars'); //template rendering, layouts, partials, helpers
 
 const routes = require('./routes');
 const sequelize = require('./config/connection.js');
 
+//sets up the Express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
 
+//set handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//middleware to handle json and url-encoded (form) data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+//starts the server to begin listening
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log("Server listening on: http://localhost:" + PORT));
 });
