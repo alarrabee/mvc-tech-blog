6b802a86afb89e81c2a82ae4844fb1492f29cc19
alarrabee <alexlarrabee10@gmail.com>
2024-07-07 18:51:30 -0500

    add associations between models

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,11 +11,7 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    content: {
+    comment_text: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
@@ -25,8 +21,8 @@ Post.init(
     timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post',
+    modelName: 'comment',
   }
 );
 
-  module.exports = Post;
+  module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,46 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { User, Post };
+
+//A user can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//A post belongs to a user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//A user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//A comment belongs to a user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//A post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+//A comment belongs to a post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+
+
+
+
+module.exports = { User, Post, Comment };
